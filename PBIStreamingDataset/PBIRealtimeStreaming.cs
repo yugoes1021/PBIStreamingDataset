ba052dd590a3bd9f2fd0f79be5df8ab4d0383dd6
Yugo Shimizu <yugoes1021@hotmail.com>
2018-06-25 12:26:37 +0900

    初期コミット

---
+++ PBIStreamingDataset/PBIRealtimeStreaming.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PBIStreamingDataset
+{
+    public class PBIRealtimeStreaming
+    {
+        public async void SendDataToPowerBI(string category, double value)
+        {
+            // Power BI REST API URL of your Streaming Dataset
+            string realTimePushURL = "<This is a Power BI REST API URL of your Streaming Dataset>";
+
+            try
+            {
+                // Current Time
+                String currentTime = DateTime.Now.ToString();
+
+                string json = "[{\"DateTime\":\"" + currentTime + "\",\"Category\":\"" + category + "\",\"Value\":" + value + "}]";
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                // Send POST request to the push URL
+                Debug.WriteLine(String.Format("Making POST request with data: {0}", content));
+
+                using (HttpClient client = new HttpClient())
+                {
+                    var response = await client.PostAsync(realTimePushURL, content);
+
+                    // Display the status.
+                    Debug.WriteLine(String.Format("Service response: {0}", response.StatusCode));
+                    // Display the content.
+                    Debug.WriteLine(response.Content);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ PBIStreamingDataset/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace PBIStreamingDataset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var category = "<Input your category name>";
+            PBIRealtimeStreaming streaming = new PBIRealtimeStreaming();
+
+            while (true)
+            {
+                Thread.Sleep(500);
+                Random random = new Random();
+                streaming.SendDataToPowerBI(category, random.Next(-10, 10));
+            }
+        }
+    }
+}
